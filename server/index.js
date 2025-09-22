f29dc0c8a825d6a5487b37b209379a1c2a80361c
OJASSharma1407 <ojas1407@gmail.com>
2025-09-23 00:55:21 +0530

    Added packages

--- server/index.js
+++ server/index.js
@@ -1,10 +1,6 @@
-
-require('dotenv').config();
 const express = require('express');
-const cron = require('./cron/remainder');
 const mongoose = require('mongoose');
 const cors = require('cors');
-const reminderJob = require('./cron/remainder');
 const port = 8000; 
 const mongo_Url = "mongodb+srv://krtyaka14_db_user:6BtmwcwlvKf6uylv@cluster0.00zy9rp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 const app = express();
@@ -18,7 +14,6 @@ mongoose.connect(mongo_Url).then(()=>{
     console.log(err);
 })
 
-reminderJob();
 
 //Routes
 app.use('/user',require('./routes/userAuth'));
